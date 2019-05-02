eb040b22dfd764871a33d5e6bcfe1b84d7b7466e
Lucas <lucasaraujola090@gmail.com>
2019-05-01 21:40:36 -0300

    atualizado

---
+++ Crossfritando/java/java.js
@@ -0,0 +1,17 @@
+ HEAD
+function validar() {
+
+  var email = document.getElementById("txtemail").value;
+
+  if (email == "#" || email.indexOf('@') == -1) {
+      alert("Encontramos um erro, por favor tente novamente!")
+      user.email.focus();
+      return false;
+  } else(
+      document.addEventListener('DOMContentLoaded', function () {
+          var elems = document.querySelectorAll('.modal');
+          var instances = M.Modal.init(elems, options);
+      }) 
+
+  )
+    }
